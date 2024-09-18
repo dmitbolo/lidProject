57519e8411bf05d375ef3a0df2bee763f45c62ff
Болотов Дмитрий <139793508+dmitbolo@users.noreply.github.com>
2024-09-19 02:29:13 +0300

    extract validation into a request file

---
+++ app/Http/Requests/LidRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LidRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'phone' => 'required|string|max:20',
+            'email' => 'required|email|max:255',
+            'text' => 'required|string',
+        ];
+    }
+}
